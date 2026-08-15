e33913391a1efad554934c9421cfccdbc9014985
Sikatu <298725931+Sikatu@users.noreply.github.com>
2026-08-14 19:35:00 -0500

    Remove legacy error reporting from public entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { installGlobalErrorReporting } from './lib/errorReporter.js'
 import '@fontsource/cormorant-garamond/latin-400.css'
 import '@fontsource/cormorant-garamond/latin-500.css'
 import '@fontsource/cormorant-garamond/latin-500-italic.css'
@@ -13,8 +12,6 @@ import '@fontsource/manrope/latin-700.css'
 import '@fontsource/manrope/latin-800.css'
 import './styles/tokens.css'
 import './styles/global.css'
-
-installGlobalErrorReporting()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
